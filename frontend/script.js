3500e73b1a82c268393cc56ed208f464c18138e3
Tanmay Hatile <gamingopgamerz@gmail.com>
2026-02-19 15:15:47 +0530

    New

--- frontend/script.js
+++ frontend/script.js
@@ -3,13 +3,22 @@ async function findRoute() {
   let end = document.getElementById("end").value;
   let friend = document.getElementById("friend").value;
 
-  let res = await fetch("https://sakti-women-safety-production.up.railway.app/route", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({start, end, friend})
-  });
+  let res = await fetch(
+    "https://sakti-women-safety-production.up.railway.app/route",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ start, end, friend }),
+    },
+  );
 
   let data = await res.json();
   document.getElementById("result").innerHTML =
     "Recommended Route: " + data.route;
+
+  var map = L.map("map").setView([20.0, 73.78], 12);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Map data © OpenStreetMap",
+  }).addTo(map);
 }
