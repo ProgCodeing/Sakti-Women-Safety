c96f7a8f59f949b9b7bc9236306a924219f18da5
Tanmay Hatile <gamingopgamerz@gmail.com>
2026-02-19 14:57:23 +0530

    New

--- backend/app.py
+++ backend/app.py
@@ -18,6 +18,7 @@ def route():
         "route": f"Safe route from {start} to {end} (traveling {friend})"
     })
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
